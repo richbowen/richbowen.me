bb458eb9ec66dbee8166233bf47fb6bd0637e59d
Richard Bowen <rich@bowenconsulting.co>
2025-03-09 22:55:26 -0500

    Update about page card layout
    Don't like using the pattern of nesting child components of a particular
    component levels below where they should be but it works in providing
    the styles I want. I may refactor later.
    
    Kael, John, is this a bad practice?

--- src/pages/about.vue
+++ src/pages/about.vue
@@ -1,12 +1,22 @@
 <template>
-  <v-card>
-    <v-img src="@/assets/richbowen.jpeg" alt="Richard Bowen" width="200" />
-    <v-card-text>
-      <p>My name is Richard Bowen and I am a software developer.</p>
-    </v-card-text>
-    <v-card-actions>
-      <v-btn href="mailto:rich@bowenconsulting.co">Contact Me</v-btn>
-    </v-card-actions>
+  <v-card class="mt-4">
+    <v-row dense>
+      <v-col cols="12" md="3">
+        <v-img src="@/assets/richbowen.png" alt="Richard Bowen" />
+      </v-col>
+      <v-col cols="12" md="9">
+        <v-card-title class="text-h6">About Me</v-card-title>
+        <v-card-text>
+          <p>
+            Product-focused, full-stack software engineer obsessed with building performant, user friendly software
+            products. Creative thinker and expert collaborator committed to continuous improvement and innovation.
+          </p>
+        </v-card-text>
+        <v-card-actions>
+          <v-btn href="mailto:rich@bowenconsulting.co">Contact Me</v-btn>
+        </v-card-actions>
+      </v-col>
+    </v-row>
   </v-card>
 </template>
 
